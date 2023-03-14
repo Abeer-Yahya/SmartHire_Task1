e2b19a43db400151be2e229ffabdd7cc008f59e4
AbeerHYahya <abeer.yahya@pwc.com>
2023-03-14 10:06:54 +0300

    creating a form and displaying user input as list item

---
+++ form.js
@@ -0,0 +1,11 @@
+// Handling Form data
+
+function addLi() {
+  var txtVal = document.getElementById("txtVal").value,
+    listNode = document.getElementById("list"),
+    liNode = document.createElement("LI"),
+    txtNode = document.createTextNode(txtVal);
+
+  liNode.appendChild(txtNode);
+  listNode.appendChild(liNode);
+}
